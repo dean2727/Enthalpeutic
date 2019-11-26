23c5be237f2ddc52a74cb854bcbb86ec2eabc108
Dean Orenstein <dto3576@gmail.com>
2019-11-26 16:53:56 -0600

    Fixed css errors, fixed JS output for warming.js, made code and webpages look cleaner, fixed the NaN issue on melting.js

--- Melting.js
+++ Melting.js
@@ -1,51 +1,55 @@
+// Function that activates the calculate function when the button is clicked
 var buttonClicked = function() {
-    console.log('Button has been clicked!')
-    
-    console.log(food)
-    $("h2").text('This is the answer !!!!')
-    calculate()
+    var food = document.getElementById("food").value;
+    var temp = document.getElementById("temp").value;
+    calculate(food, temp)
 }
 
-var calculate = function(temp_f) {
-    var farToCel = function(temp_f){
-        var temp_C = (temp_f - 32) * (5/9)
-        return temp_C
-    }
-    var food = $("#food").val()
-    var temp = (temp_f - 32) * (5/9)
-    if (food.toLowerCase() == 'ice cream') {
-        var mass = 568 * 5.9147065 * (10 **-5) //kg
-        //Q = MCAT
-        var heat = (mass * 1.76 * 100 * 18) +(210.14 * mass) 
+var farToCel = function(temp_f) {
+    var temp_C = (temp_f - 32) * (5/9)
+    return temp_C
+}
+
+// Uses physics calculations to get length of time in minutes for food to start melting
+var calculate = function(food, temp) {
+    var temp = farToCel(temp);
+
+    if (food.toLowerCase() == 'icecream' || food.toLowerCase() == 'ice cream') {
+        var mass = 568 * 5.9147065 * (10 **-5)  // in kg
+        // Q = MCdeltaT
+        var heat = (mass * 1.76 * 100 * 18) + (210.14 * mass) 
         //J __ Ice cream melts at 0, most freezers at -18
-        var pow = (5.6703*Math.pow(10 , -8)) * (0.0126677 ) * Math.pow(((temp + 275.15), 4) - Math.pow((273.15) , 4))
+        var pow = 5.6703*Math.pow(10,-8) * 0.0126677 * Math.pow(temp+275.15,4) - Math.pow(273.15,4)
         var time = Math.round(Math.pow(((heat/pow)),2))
-        $("h2").text("Your ice cream will start melting in " + time + " seconds" )
-        //pow in watts
+        console.log(mass, heat, pow, time)
+        $("h2").text("Your icecream will start melting in " + time + " seconds" )
     }
+
     else if (food.toLowerCase() == 'chocolate'){
-        if (temp_f <=84){
-            console.log("Your chocolate won't melt!")
+        if (temp_f <= 86){
+            $("h2").text("Your chocolate won't melt!")
         }
-        else{
-            var mass =  0.001881984 * 1325 //kg __ density = 1325 kg/m3
-            var heat = (mass * 1.589 * (farToCel(85)-farToCel(60))) + (mass * 311.2) //Specific heat: 1.589 KJ/KgC
-            var pow =(5.6703*(Math.pow(10 , -8))) * (.0099) * Math.abs(Math.pow((273.15+temp) , 4)) - Math.pow(273.15+(farToCel(85)) , 4)
-            var time = Math.round(((heat/pow)/60 * 100) / 100)
-            console.log(time)
-            $("h2").text("Your chocolate bar will start melting in " + time + " minutes" )
+        else {
+            // kg __ density = 1325 kg/m3
+            var mass =  0.001881984 * 1325
+            // Specific heat: 1.589 KJ/KgC
+            var heat = (mass * 1.589 * (farToCel(85) - farToCel(60))) + (mass * 311.2)
+            var pow =(5.6703*(Math.pow(10,-8))) * 0.0099 * Math.abs(Math.pow((273.15+temp),4)) - Math.pow(273.15+(farToCel(85)),4)
+            var time = Math.round((((heat/pow)/60)*100) / 100)
+            $("h2").text("Your chocolate will start melting in " + time + " minutes" )
         }
     }
-    else if (food.toLowerCase() == 'ice'){
-        var mass = (Math.pow((2.5/100), 3) * 917)
-        var heat = ((mass * 2.10) * 18) + (mass * 333.55)
-        var pow = ((5.6703*Math.pow(10 , -8)) * Math.pow(3.75*10 , -3) * (Math.pow((temp + 275.15) , 4) - Math.pow((273.15) , 4)))
-        var time = Math.round(((heat/pow)),2) 
-        $("h2").text("Your ice cubes will start melting in " + time + " seconds" )
+    
+    else if (food.toLowerCase() == 'ice') {
+        var mass = Math.pow((2.5/100),3) * 917
+        var heat = mass * 2.10 * 18 + mass * 333.55
+        var pow = 5.6703*Math.pow(10,-8) * Math.pow(3.75*10,-3) * Math.pow((temp + 275.15),4) - Math.pow(273.15,4)
+        var time = Math.round(heat/pow,2)
+        console.log(mass, heat, pow, time)
+        $("h2").text("Your ice will start melting in " + time + " seconds" )
     }
+
     else {
         $("h2").text("Please enter one of the specified foods")
     }
-    //food = str(input("Do you have 'ice cream', 'chocolate', or 'ice'? "))
-    //temp_f = float(input("What's the temperature of the room in farenheit? "))
 }
--- Warming.js
+++ Warming.js
@@ -2,35 +2,31 @@
 var buttonClicked = function() {
     var volume = document.getElementById("volume").value;  // Gets the value in the text field
     var ice = document.getElementById("ice").value;
-    var insolation = document.getElementById("insulation").value;
+    var insulation = document.getElementById("insulation").value;
     calculate(volume, ice, insulation)
+}
+
+var farToCel = function(temp_f) {
+    var temp_C = (temp_f - 32) * (5 / 9)
+    return temp_C
 }
 
 // Uses physics calculations to get length of time in minutes for water to reach room temperature
 var calculate = function(volume, ice, insulation) {
-    var mass_ice = 4  // Assuming mass of ice is 4 grams
-    var vol = volume
-    var prop_ice = ice
-    var insul = insulation
-    var height_in = (vol / 100)
-    var mass_water = vol * (1 - prop_ice) * vol
+    var mass_ice = 28  // A typical ice cube is 28 grams
+    var height_in = (volume / 100)
+    var mass_water = volume * (1 - ice) * volume
     var heat_water = (mass_water) * 4.184 * (68.4)
     var heat_ice = ((mass_ice * 2.05) * 18) + (mass_ice * 333.55) 
-    var mass_melted_ice = (vol * prop_ice)
+    var mass_melted_ice = (volume * ice)
     var heat_melted_ice = mass_melted_ice * 4.184 * (70) 
-
-    var farToCel = function(temp_f) {
-        var temp_C = (temp_f - 32) * (5 / 9)
-        return temp_C
-    }
-
     var surface_area = (3.14159 * 2) * (2 * 3.14159 * height_in * 0.0254)
-
-    var tot_heat = (heat_ice + heat_melted_ice + heat_water) * (1 / (1 - insul))
+    var tot_heat = (heat_ice + heat_melted_ice + heat_water) * (1 / (1 - insulation))
     var tot_heat_kj = tot_heat / 1000
     var power = ((5.6703 * (10 ** -8)) * .1 * (surface_area) * Math.abs((Math.pow((273.15 + farToCel(70))) , 4) 
     - Math.pow((273.15), 4) / 5))
     var time = Math.round(((tot_heat_kj/power)/60),3)
-    console.log()
+
+    // The text for the h2 element will say the follwing once the calculations are done
     $("h2").text("Your water will be room temperature in " + time + " minutes")
 }
